9c5abdf9b2421e3a5456bf250c287ec516fc1122
Kostiantyn Zhereb <zhereb@gmail.com>
2020-01-28 12:15:21 +0200

    Trying to implement editing, still doesn't work (not all items are written back into new file)

--- src/lesson_k19_2020_01_28.cpp
+++ src/lesson_k19_2020_01_28.cpp
@@ -46,12 +46,29 @@ void refresh_file(std::string file_path) {
 }
 
 void edit_text_file(std::string file_path, int id, Person person) {
-	std::fstream file(file_path);
+	std::ifstream old_file(file_path);
+	std::ofstream new_file(file_path+".new.txt");
 	std::string line;
-	while(getline(file,line)) {
-		int line_id = std::stoi(line);
+	bool ignore_lines = false;
 
+	while(getline(old_file,line)) {
+		if (line.substr(0, 2) == "id") {
+			int line_id = std::stoi(line.substr(3));
+			if (id == line_id) {
+				std::cout<<"found "<<line<<std::endl;
+				person.id = id;
+				write_text_file(person, file_path+".new.txt");
+				ignore_lines = true;
+			} else {
+				ignore_lines = false;
+				new_file << line <<std::endl ;
+			}
+		} else if (! ignore_lines){
+			new_file << line <<std::endl ;
+		}
 	}
+	old_file.close();
+	new_file.close();
 
 }
 
@@ -61,6 +78,7 @@ int main() {
 	std::cout<<"file refreshed"<<std::endl;
 	write_text_file(Person("First Person",5,50,Gender::GMAIL),"text.txt");
 	write_text_file(Person("Second Person",50,5,Gender::GMAIL),"text.txt");
+	edit_text_file("text.txt",0,Person("First Person",5,55,Gender::MALE));
 	return 0;
 }
 
