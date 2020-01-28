4a90acbbc411aba7c6a310c1b6841d73f476124b
Kostiantyn Zhereb <zhereb@gmail.com>
2020-01-28 11:34:08 +0200

    Implemented id, autoincrement

--- src/lesson_k19_2020_01_28.cpp
+++ src/lesson_k19_2020_01_28.cpp
@@ -11,6 +11,8 @@
 enum Gender { MALE, FEMALE, GMAIL };
 
 struct Person {
+	int id;
+	static int max_id;
 	std::string name;
 	int age;
 	double height; // in cm
@@ -21,11 +23,16 @@ struct Person {
 		this->age = age;
 		this->height = height;
 		this->gender = gender;
+		this->id = max_id;
+		max_id++;
+
 	}
 };
+int Person::max_id = 0;
 
 void write_text_file(Person person, std::string file_path) {
 	std::ofstream file(file_path);
+	file << person.id << std::endl;
 	file << person.name << std::endl;
 	file << person.age << std::endl;
 	file << person.height << std::endl;
@@ -33,9 +40,14 @@ void write_text_file(Person person, std::string file_path) {
 	file.close();
 }
 
+void edit_text_file(std::string path, Person person) {
+
+}
+
 int main() {
 	std::cout<<"hello world"<<std::endl;
 	write_text_file(Person("First Person",5,50,Gender::GMAIL),"text.txt");
+	write_text_file(Person("Second Person",50,5,Gender::GMAIL),"text.txt");
 	return 0;
 }
 
