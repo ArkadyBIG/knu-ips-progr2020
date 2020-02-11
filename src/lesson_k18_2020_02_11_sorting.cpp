f5698653737e696c9c8c6f485c29c541d251ab79
Kostiantyn Zhereb <zhereb@gmail.com>
2020-02-11 10:11:43 +0200

    Insertion sort
    Borrowed from wikipedia as well, fixed while->for

--- src/lesson_k18_2020_02_11_sorting.cpp
+++ src/lesson_k18_2020_02_11_sorting.cpp
@@ -5,10 +5,20 @@
  *      Author: KZ
  */
 #include <iostream>
+#include <algorithm>
 #include <cstddef>
 
 namespace lesson_k18_2020_02_11_sorting {
 
+
+void insertion_sort(int* array, std::size_t size) {
+
+	for (std::size_t i = 1; i < size; i++) {
+		for (std::size_t j = i; j>0 && array[j-1]>array[j];j--) {
+			std::swap(array[j],array[j-1]);
+		}
+	}
+}
 
 void shell_sort(int* array, std::size_t size) {
 	static const int gaps[] = {701, 301, 132, 57, 23, 10, 4, 1 };
@@ -31,7 +41,8 @@ int main() {
 
 	int test_array[] = {2,74,1,-43, 23, 123, 55, -4, 0, 1};
 	std::size_t size = sizeof(test_array)/sizeof(test_array[0]);
-	shell_sort(test_array, size);
+	//shell_sort(test_array, size);
+	insertion_sort(test_array, size);
 	for(auto i: test_array) {
 		std::cout<<i<<" ";
 	}
