44468bbd6c561dbf0d9d345027bcf91d86bad3c0
Kostiantyn Zhereb <zhereb@gmail.com>
2020-02-11 10:36:20 +0200

    Slightly better implementation of insertion sort
    Don't swap items (like in bubble sort), move them once and put current
    item in correct place

--- src/lesson_k18_2020_02_11_sorting.cpp
+++ src/lesson_k18_2020_02_11_sorting.cpp
@@ -11,12 +11,22 @@
 namespace lesson_k18_2020_02_11_sorting {
 
 
-void insertion_sort(int* array, std::size_t size) {
-
+void insertion_sort_naive(int* array, std::size_t size) {
 	for (std::size_t i = 1; i < size; i++) {
 		for (std::size_t j = i; j>0 && array[j-1]>array[j];j--) {
 			std::swap(array[j],array[j-1]);
 		}
+	}
+}
+
+void insertion_sort_smarter(int* array, std::size_t size) {
+	for (std::size_t i = 1; i < size; i++) {
+		int temp = array[i];
+		std::size_t j;
+		for (j = i; j>0 && array[j-1]>temp;j--) {
+			array[j] = array[j-1];
+		}
+		array[j] = temp;
 	}
 }
 
@@ -42,7 +52,7 @@ int main() {
 	int test_array[] = {2,74,1,-43, 23, 123, 55, -4, 0, 1};
 	std::size_t size = sizeof(test_array)/sizeof(test_array[0]);
 	//shell_sort(test_array, size);
-	insertion_sort(test_array, size);
+	insertion_sort_smarter(test_array, size);
 	for(auto i: test_array) {
 		std::cout<<i<<" ";
 	}
