740c4c62bdb4873187208b12bc1a59ad62206545
Kostiantyn Zhereb <zhereb@gmail.com>
2020-03-03 11:34:41 +0200

    Trying to implement sparse list based on singly linked list

---
+++ src/lesson_k19_2020_03_03_sparse_list.cpp
@@ -0,0 +1,76 @@
+/*
+ * lesson_k19_2020_03_03_sparse_list.cpp
+ *
+ *  Created on: Mar 3, 2020
+ *      Author: KZ
+ */
+
+#include <iostream>
+
+namespace lesson_k19_2020_03_03_sparse_list {
+
+
+struct SparseListNode {
+	int value;
+	std::size_t index;
+
+	SparseListNode* next;
+	SparseListNode (int value, std::size_t index, SparseListNode* next=nullptr):
+		value(value), index(index), next(next) {}
+};
+
+struct SparseList {
+	std::size_t size;
+	std::size_t nonempty_size;
+	SparseListNode* head;
+
+	SparseList(std::size_t size): size(size), nonempty_size(0), head(nullptr) {}
+	void print_with_index() {
+		for(SparseListNode* current = head; current; current = current->next) {
+			std::cout<<current->index<<": "<<current->value<<std::endl;
+		}
+	}
+
+	void insert(int value, std::size_t index) {
+		if (index>size) {
+			throw std::invalid_argument("Index greater than size");
+		}
+		SparseListNode* current = head;
+		if (current == nullptr) {
+			head = new SparseListNode(value, index);
+			size++;
+			nonempty_size++;
+			return;
+		}
+		std::size_t current_position = 0;
+		while (current) {
+			if (current_position>index) {
+				break;
+			}
+			if(current->next) {
+				current = current->next;
+			}
+			current_position++;
+		}
+
+		SparseListNode* new_node = new SparseListNode(value, index, current->next);
+		if (current == nullptr) {
+
+		}
+		current->next = new_node;
+
+	}
+};
+
+
+int main() {
+	SparseList list(100);
+
+	list.insert(10, 3);
+	list.insert(-12, 10);
+	list.print_with_index();
+	return 0;
+}
+
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
  *  Created on: Feb 8, 2020
  *      Author: KZ
  */
-#define current_ns lesson_k18_2020_02_11_sorting
+#define current_ns lesson_k19_2020_03_03_sparse_list
 
 namespace current_ns {
 	int main();
